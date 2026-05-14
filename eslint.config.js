026eed77eb04146986646a67c41c2d8ceed373b5
OktalonSzoradi <oktalon.szoradi@stud.hcw.ac.at>
2026-05-14 16:53:12 +0200

    Migrate to neostandard, adjust some things

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+// import biome from 'eslint-config-biome'
+import neostandard from 'neostandard'
+
+export default [...neostandard({})]
---
+++ js/allTheErrors.js
@@ -0,0 +1,371 @@
+(() => {
+   console.log('meow')
+})()
+
+console.log("hello there")
+console.log(`hello there`)
+
+const unused = 'meow'
+let meow = 'mrrrp'
+
+if(meow) { meow = ':3' }
+
+function meoww() { console.log('nyaaa') }
+meoww()
+
+if (meow == 'OwO') {
+  if (meow != 'bark') {
+    console.log('>w<')
+  }
+}
+
+meow='miau'
+
+meow = ['meow','mreow','mrrrp']
+;((a,b,c) => {
+  console.log(meow)
+})()
+
+if (!meow) {
+  console.log('meow is gone!!  qwq')
+}
+else {
+  // …
+}
+
+if (meow)
+  console.log(':3c')
+
+if (meow === 'mrrp') {
+  // meow
+}
+
+;(function (err) {
+  window.alert('done')
+})()
+
+alert('MEOW!!')
+
+
+// meow
+const mrrrp = meow ?
+  'mrrrp' :
+  '*airplane ears*'
+console.log(mrrrp)
+
+var owo = 'UwU', miau = true
+
+while (meow = 1) {
+  console.log(meow)
+}
+
+function meowww () {return true}
+meowww()
+
+const meow_meow_meow = meow
+
+meow = {
+  mrrp: 'ówò',
+}
+
+meow = {
+  mrrp: 'ówò'
+  ,mrrrrp: 'UwU'
+}
+
+console.
+  log('Meoww!')
+
+console.log ('OwO')
+
+meow = { OwO : meow }
+
+const meowwww = new meowww()
+console.log(meowwww)
+
+function Meowww () {}
+const meowwwww = new Meowww
+console.log(meowwwww)
+
+const cat = {
+  set name (value) {
+    this._name = value
+  }
+}
+console.log(cat)
+
+class Cat {
+  constructor () {
+    this.meow = 'mrrrp'
+  }
+}
+
+class MeowMeow extends Cat {
+  constructor () {
+
+  }
+}
+const mrrpMeow = new MeowMeow()
+console.log(mrrpMeow)
+
+const owoArray = new Array(1, 2, 3)
+console.log(owoArray)
+
+function foo (n) {
+  if (n <= 0) return
+
+  arguments.callee(n - 1)
+}
+foo()
+
+MeowMeow = 'zzz'
+
+meow_meow_meow = 'miaumiaumiau'
+
+if (true) { console.log('Nyaa~!  >w<') }
+
+const pattern = /\x1f/
+console.log(pattern)
+
+debugger
+
+// delete owo
+
+// function nyaaa (meow, mrrrp, meow) {}
+
+class Cat2 {
+  meow () {}
+  meow () {}
+}
+const cat2 = new Cat2()
+console.log(cat2)
+
+console.log(
+  {
+    meow: 'mrrp',
+    meow: 'mrrp'
+  }
+)
+
+switch (meow) {
+  case 'meow':
+  case 'meow':
+}
+
+const myRegex = /^abc[]/
+console.log(myRegex)
+
+const { a: {} } = foo
+
+eval('var result = user.' + meow)
+
+try {
+  console.log('mreow')
+} catch (e) {
+  e = 'MEOW'
+}
+
+Object.prototype.meow = ':3'
+
+const name = function () {
+  meow()
+}.bind(meow)
+console.log(name)
+
+if (!!meow) console.log('meow')
+
+const mreow = (function () { })
+mreow()
+
+switch (meow) {
+  case 1:
+    meow()
+  case 2:
+    mreow()
+}
+
+switch (meow) {
+  case 1:
+    meow()
+    // fallthrough
+  case 2:
+    mreow()
+}
+
+const MEOW = .1
+console.log(MEOW)
+
+Meowww = 'OwO'
+
+window = 'meow'
+
+setTimeout("alert('MEOW')")
+
+if (meow) {
+  function meowowo () {} // no-inner-declarations??????
+  meowowo()
+}
+
+RegExp('[a-z')
+
+Foo.prototype.__iterator__ = function () {}
+
+let score = 100
+function game () {
+  score: while (true) {
+    score -= 10
+    if (score > 0) continue score
+    break
+  }
+}
+game()
+
+function myFunc () {
+  {
+    meow()
+  }
+}
+myFunc()
+
+meow  = 1
+
+new MeowMeow()
+
+console.log(new Object())
+
+console.log(new require('my-module'))
+
+console.log(new Symbol('foo'))
+
+console.log(new String('meow'))
+
+console.log(Math())
+
+// console.log(0o42, '\251')
+
+console.log(meow.__proto__)
+
+// let meow = 'UwU'
+
+;(function UwU (a, b) {
+  return owo = a + b
+})()
+
+meow = meow
+
+if (meow === meow) {
+  console.log('>w<')
+}
+
+if (meow, meoww) {
+  console.log('mrrrrp')
+}
+
+let undefined = 'OwO'
+
+console.log(['meow',, 'UwU'])
+
+console.log('Haiii ${UwU}')
+
+class Mrrrrp extends Cat {
+  constructor () {
+    this.owo = '>w<'
+    super()
+  }
+}
+
+try {
+  throw new Response()
+  throw 'Meow!!'
+} catch (meow) {
+  console.log(meow)
+}
+
+meow = 'UwU' 
+
+const meowUwU = undefined
+console.log(meowUwU)
+
+for (let i = 0; i < 10; j++) {
+  console.log('mrrrrp~')
+}
+
+const UwU = meow ? meow : 0
+console.log(UwU)
+
+;(() => {
+  return
+  console.log('UwU')
+})()
+
+;(() => {
+  try {
+    console.log('meow')
+  } catch (e) {
+    console.log(e)
+  } finally {
+    return 3
+  }
+})()
+
+if (!meow in meow) { console.log('OwO') }
+
+meow.call(null, 1, 2, 3)
+meow.apply(null, 1, 2, 3)
+
+console.log({ ['name']: 'John Doe' })
+
+console.log(
+  class OwO {
+    constructor () {
+
+    }
+  }
+)
+
+console.log('Me\ow')
+
+meow .meow = 'UwU'
+
+// with (meow) {
+//   console.log(UwU)
+// }
+
+const meowmeowmeow = {
+  meow: 'mrrrp', cat: true,
+  owo: 'nyaaa~'
+}
+
+if (meow) {
+  console.low(meowmeowmeow)
+
+}
+
+console.log(... meow)
+
+if (meow){
+  console.log('meow')
+}
+
+console.log(typeof!meow)
+
+//meow
+
+console.log(`Haiii ${ UwU }`)
+
+if (meow === NaN) {
+  meow()
+}
+
+console.log(typeof meow === 'meow')
+
+const getMeow = function () { }()
+console.log(getMeow)
+
+// yield* meow()
+
+if (42 === meow) {
+  console.log('UwU')
+}
+
+meow();
